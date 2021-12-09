850790e0a528032561512cabe9bee870fb90cb6a
ZeroByteOrg <thatguy@zerobyte.org>
2021-12-09 17:49:12 -0600

    Digi playback test works

--- examples/pcmtest/pcmtest.asm
+++ examples/pcmtest/pcmtest.asm
@@ -4,10 +4,12 @@
 ; ZP variables potentially used in zsound
 pcm_ptr:		.res 2
 pcm_bank:		.res 1
+pcmbytes:		.res 2
+
+
 end_ptr:		.res 2
 end_bank:		.res 1
 
-pcmbytes:		.res 2
 pcmblit:		.res 2
 ;active_digi:	.res 1	; moved to BSS for now
 
@@ -68,11 +70,37 @@ digi:			.tag DIGITAB
 ; test shell
 ;---------------------------------------------------------------
 
+;.segment "ONCE"
+.segment "RODATA"
+filename:	.byte "raw2"
+filename_len = (*-filename)
+
 .segment "STARTUP"
-	jmp	init
-	jmp coin_sound
+
+	DIGI_BANK = 1
+	LOADTO = $a000
+	
+	; set BANKRAM to the first bank where song should load
+	lda	#DIGI_BANK
+	sta	RAM_BANK
+	; prepare for call to SETNAM Kernal routine
+	lda #filename_len
+	ldx #<filename
+	ldy #>filename
+	jsr SETNAM
+	; prepare for call to SETLFS Kernal routine
+	lda #0	; logical file id 0
+	ldx	#8	; device 8
+	ldy #0	; 0 = no command
+	jsr	SETLFS
+	; load song to LOADTO
+	lda	#0		; 0=load, 1=verify, 2|3 = VLOAD to VRAM bank0/bank1
+	ldx	#<LOADTO
+	ldy #>LOADTO
+	jsr LOAD
+	
+	jsr	init
 	jmp startup_tune
-	rts
 	
 coin_sound:
 	ldy #0
@@ -84,28 +112,16 @@ coin_sound:
 	jsr start_digi
 	rts
 
-;startup_tune:
-;	ldy #1
-;	lda pcmtab_lo,y
-;	tax
-;	lda pcmtab_hi,y
-;	tay
-;	lda #1
-;	jsr start_digi
-;	rts
-
 startup_tune:
-	ldx pcmtab_lo+1
-	stx pcmbytes
-	ldx pcmtab_hi+1
-	stx pcmbytes+1
-	ldy DIGITAB_LAST
-:	lda (pcmbytes),y
-	sta digi,y
-	dey
-	bpl :-
-	;;;; continue here
-	
+	ldy #1
+	lda pcmtab_lo,y
+	tax
+	lda pcmtab_hi,y
+	tay
+	lda #1
+	jsr start_digi
+	rts
+
 ;---------------------------------------------------------------
 ; begin zsound PCM player module candidate code
 ;---------------------------------------------------------------
@@ -123,6 +139,7 @@ irqhandler:
 	cmp IRQVec
 	bne install_irq
 	lda #>irqhandler
+	cmp IRQVec+1
 	beq	done
 install_irq:
 	sei
@@ -152,7 +169,7 @@ done:
 	stx	NEWDIGI_PTR
 	sty NEWDIGI_PTR+1
 	tax
-	jsr stop_pcm	; do we really need a full STOP to go by? Probably not.
+	jsr stop_pcm
 	lda RAM_BANK
 	sta BANK_SAVE
 	stx RAM_BANK
@@ -176,9 +193,7 @@ loop:
 	sta VERA_audio_ctrl
 	; pre-load the FIFO
 	dec active_digi
-	jsr play_pcm	; preload 2 frames' worth of samples into the FIFO.
-	;jsr play_pcm
-	;jsr play_pcm
+	jsr play_pcm	; prime the FIFO with at least 1 frame's worth of data.
 	; enable VERA PCM playback
 	lda digi + DIGITAB::rate
 	sta VERA_audio_rate
@@ -192,11 +207,14 @@ loop:
 .segment "CODE"
 .proc play_pcm: near
 
-	totalbytes = digi + DIGITAB::size
-	bytesperframe = digi + DIGITAB::perframe
+	totalbytes		= digi + DIGITAB::size
+	bytesperframe 	= digi + DIGITAB::perframe
+	digi_addr		= digi + DIGITAB::addr
+	digi_bank		= digi + DIGITAB::bank
 	
 	lda	active_digi		; quick check whether digi player is active
-	beq done
+	clc
+	beq noop
 	lda totalbytes		; check whether any bytes remain in the digi
 	ora totalbytes+1
 	ora totalbytes+2
@@ -205,27 +223,36 @@ end_of_digi:
 	stz active_digi		; ... if not, then deactivate digi player
 	rts
 :	sec
+	; configure the ZP pointer for load_fifo
+	lda digi_addr
+	sta pcm_ptr
+	lda digi_addr+1
+	sta pcm_ptr+1
+	lda digi_bank
+	sta pcm_bank
 	; call load_fifo with the lesser of totalbytes or bytesperframe
 	lda totalbytes
 	sbc bytesperframe
 	lda totalbytes+1
 	sbc bytesperframe+1
-	bcc send_totalbytes
-send_bytesperframe:
-	lda bytesperframe
-	sta pcmbytes
-	lda bytesperframe+1
-	sta pcmbytes+1
-	bra call_load_fifo
+	bcs send_bytesperframe
 send_totalbytes: ; i.e. the last frame's worth of samples.
-	lda totalbytes
-	sta pcmbytes
-	lda totalbytes+1
-	sta pcmbytes+1
+	ldx totalbytes
+	ldy totalbytes+1
+	bra call_load_fifo
+send_bytesperframe:
+	ldx bytesperframe
+	ldy bytesperframe+1
 call_load_fifo:
 	jsr	load_fifo
 	php
-	; subtract bytes transferred (pcmblit) from totalbytes
+	; update the pcmtab 
+	lda pcm_ptr
+	sta digi_addr
+	lda pcm_ptr+1
+	sta digi_addr+1
+	lda pcm_bank
+	sta digi_bank
 	sec
 	lda totalbytes
 	sbc pcmblit
@@ -236,10 +263,13 @@ call_load_fifo:
 	lda totalbytes+2
 	sbc #0
 	sta totalbytes+2
+	bpl done
+	brk
+	stz active_digi
 	; note: wouldn't carry=clear here mean the digi is done?
-	plp
-	bcs	call_load_fifo
 done:
+	plp
+noop:
 	rts
 .endproc
 
@@ -247,101 +277,90 @@ done:
 ;---------------------------------------------------------------
 ; load_fifo: 
 ;
+; Copies PCM data into FIFO - stops when either max bytes are copied
+; or if the FIFO becomes full. Returns number of bytes copied.
+;
 ; Arguments:
-;	.X/.Y = starting address
+;	.X/.Y = max bytes to copy
 ;
-; Setup:	Before calling, the current source RAM bank should be active
-;			and the ZP variables end_ptr,end_bank should be set.
+; Returns:
+;	CC if complete. CS if stop early for any reason.
+;	Sets pcmblit = number of bytes written into FIFO.
 ;
-; Affects:	If a bank wrap occurs, the current source RAM bank will
-;			be updated.
-;			
-; Returns:	CC if complete. CS if stop early for any reason.
-;			Modifies the digi's pointer in BSS memory (digi::addr)
+; Setup:
+;	Before calling, the ZP variables pcm_ptr and pcm_bank should be set.
 ;
-; Copies PCM data into FIFO from starting address up to and including
-; an ending address specified in ZP. Updates the digi information's
-; pointer to point at the first un-copied byte.
+; Affects:
+;	ZP variable pcmblit is used to store the remaining byte countdown.
 ;
-; Exits early with noop if dst page >= $c0
-; Exits early if FIFO becomes full
 ;---------------------------------------------------------------
 .segment "CODE"
 .proc load_fifo: near
 
-.macro NEXTPAGE
-	inx
-	cpx #$c0
-	bne @nowrap
-	ldx #$a0
-	inc RAM_BANK
-@nowrap:
-	stx pcm_ptr+1
-.endmacro
-
-	lda #$c0		; avoid infinite loop from end ptr above bank window
-	cmp	end_ptr+1
-	bcs noop
-	lda end_ptr
-	sta STOPVAL
-	; initialize pcm_ptr aligned to current page.
-	stz pcm_ptr
-	stx pcm_ptr+1
-	bra continue
+	; .XY = num bytes to copy
+	; save as bytes_in, and copy to pcmblit (workspace in ZP)
+	stx bytes_in
+	sty bytes_in+1
+	sty pcmblit+1
+	ldy pcm_ptr
+	stz pcm_ptr		; page-align the data pointer
+	lda RAM_BANK
+	sta BANK_SAVE
+	lda pcm_bank
+	sta RAM_BANK
+	bra copy_byte
 loop:
-	iny
-	bne continue
-	NEXTPAGE
-continue:
-	lda #$08
-	and VERA_isr	; is FIFO full?
-	bne stop_load
+	dex
+	bne copy_byte
+	lda pcmblit+1
+	clc				; clear carry for return status in case we're done
+	beq done
+	dec
+	sta pcmblit+1
+copy_byte:
+	bit VERA_audio_ctrl	; check FIFO full bit
+	bmi early_exit
 	lda (pcm_ptr),y
 	sta VERA_audio_data
-	; check whether this was the last byte to copy
-	cpy #$FF
-	STOPVAL = (*-1)	; self-mod target for the end address low-byte
-	bne loop
-	cpx	end_ptr
-	bne loop
-	lda end_bank
-	cmp RAM_BANK
-	bcc loop
-	; point at the first byte after the final byte copied.
+	nop
+	; advance pointer to the next byte, do bank wrap if necessary
 	iny
-	bne done
-	NEXTPAGE
-done:
-	clc
-exit:
-	sty digi + DIGITAB::addr
-	stx digi + DIGITAB::addr+1
-	lda RAM_BANK
-	sta digi + DIGITAB::bank
-	clc
-noop:
-	rts
-stop_load:
-	sec
-	bra exit
-.endproc
+	bne loop
+	lda pcm_ptr+1
+	inc
+	cmp #$c0
+	bne nowrap
+	lda #$a0
+	inc RAM_BANK
+nowrap:
+	sta pcm_ptr+1
+	bra loop
 
-;---------------------------------------------------------------
-; This code might not be used after the new load_fifo is done...
-; remember to delete it if unused.
-.segment "CODE"
-.proc test_EOD: near
-	lda	pcmbytes
-	bne notfinished
-	ora pcmbytes+1
-	bne notfinished
-finished:
-	clc
-	rts
-notfinished:
+early_exit:
 	sec
+done:
+	php
+	stx pcmblit
+	sty pcm_ptr
+	lda RAM_BANK
+	sta pcm_bank
+	lda #$FF
+	BANK_SAVE = (*-1)
+	sta RAM_BANK
+	sec
+	lda bytes_in
+	sbc pcmblit
+	sta pcmblit
+	lda bytes_in+1
+	sbc pcmblit+1
+	sta pcmblit+1
+	plp
 	rts
-.endproc
+
+bytes_in:	.res 2
+
+.endproc	
+
 
 ;---------------------------------------------------------------
 .segment "CODE"
@@ -350,7 +369,5 @@ notfinished:
 	lda #$80
 	sta VERA_audio_ctrl
 	stz active_digi
-	stz pcmbytes
-	stz pcmbytes+1
 	rts
 .endproc
