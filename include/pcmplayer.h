cdba8c218c614342a98030c1e9c6087bc48a2650
ZeroByteOrg <thatguy@zerobyte.org>
2022-06-09 18:14:08 -0500

    forgot #endif in pcmplayer.h

--- include/pcmplayer.h
+++ include/pcmplayer.h
@@ -14,3 +14,5 @@ extern void __fastcall__ pcm_trigger_digi(uint8_t bank, uint16_t addr);
 extern void __fastcall__ pcm_play();
 extern void __fastcall__ pcm_stop();
 extern void __fastcall__ pcm_set_volume(uint8_t volume);
+
+#endif
