6e368de0db0eac32af8446197e78e1c53548b09e
Irmen de Jong <irmen@razorvine.net>
2022-06-21 00:01:30 +0200

    add optional custom output file to makezcm, and refactorings

--- tools/makezcm.py
+++ tools/makezcm.py
@@ -1,62 +1,114 @@
 #!/usr/bin/env python
 
 # Convert source audio file into ZCM digi file.
-# Requires python 'sox' module to be installed, and the sox command line tool.
+# Requires python 'sox' module to be installed (pip install sox), and the sox command line tool.
 
 import os
-import sys
 import struct
 import tempfile
 import sox
 import argparse
 
 
-MAX_SIZE = 2048*1024 - 8192    #  2 mb banked ram minus 1 bank (8kb) for kernal.
+class ZcmHeader():
+    # note: this class is shared with other Zcm tools
+    MAX_SIZE = 2048 * 1024 - 8192  # 2 mb banked ram minus 1 bank (8kb) for kernal.
+    HEADER_FORMAT = "<HBHBBB"
+
+    def __init__(self) -> None:
+        self.sample_rate = 0
+        self.bits = 0
+        self.channels = 0
+        self.size = 0
+        self.volume = 0x0f
+        self.duration = 0.0
+        self.header_bytes = b""
+
+    @staticmethod
+    def from_config(samplerate: int, bits: int, channels: int, size: int) -> 'ZcmHeader':
+        assert bits in (8, 16)
+        assert channels in (1, 2)
+        assert 1 <= samplerate <= 48828
+        assert 0 < size <= ZcmHeader.MAX_SIZE
+        header = ZcmHeader()
+        vera_rate = header.samplerate_to_vera_rate(samplerate)
+        vera_16_bit = 1 << 5 if bits == 16 else 0
+        vera_stereo = 1 << 4 if channels == 2 else 0
+        header.sample_rate = header.vera_rate_to_samplerate(vera_rate)
+        header.bits = bits
+        header.channels = channels
+        header.size = size
+        header.volume = 0x0f
+        header.duration = size / channels / header.sample_rate
+        if bits == 16:
+            header.duration /= 2
+        header.header_bytes = struct.pack(ZcmHeader.HEADER_FORMAT, 0, 0, size & 0xffff, size >> 16,
+                                          vera_16_bit | vera_stereo | header.volume, vera_rate)
+        return header
+
+    @staticmethod
+    def from_header(header_bytes: bytes) -> 'ZcmHeader':
+        addr, bank, size_lo, size_hi, vera_cfg, vera_rate = struct.unpack(ZcmHeader.HEADER_FORMAT, header_bytes)
+        if addr or bank:
+            print("Warning: the first 3 bytes of the zcm aren't zero! (addr/bank)")
+        header = ZcmHeader()
+        header.size = size_lo + (size_hi << 16)
+        header.bits = 16 if vera_cfg & (1 << 5) else 8
+        header.channels = 2 if vera_cfg & (1 << 4) else 1
+        header.volume = vera_cfg & 0x0f
+        header.header_bytes = header_bytes
+        header.sample_rate = header.vera_rate_to_samplerate(vera_rate)
+        header.duration = header.size / header.channels / header.sample_rate
+        if header.bits == 16:
+            header.duration /= 2
+        return header
+
+    @staticmethod
+    def samplerate_to_vera_rate(hz: int) -> int:
+        return int((hz / (25e6 / 65536)) + 1)
+
+    @staticmethod
+    def vera_rate_to_samplerate(vrate: int) -> int:
+        return int(vrate * (25e6 / 65536))
+
 
 def convert_source_to_raw(source: str, outputformat: dict) -> str:
     info = sox.file_info.info(source)
-    print("Source file: ",source)
+    print(f"Source file: {source}")
     print(f"   {info['channels']} channels, {info['bitdepth']} bits, {int(info['sample_rate'])} hz ({round(info['duration'], 3)} sec.)")
     tfm = sox.Transformer()
-    tfm.set_output_format(file_type="raw", encoding = "signed-integer" if outputformat["bits"]>8 else "signed-integer")
+    tfm.set_output_format(file_type="raw", encoding="signed-integer" if outputformat["bits"] > 8 else "signed-integer")
     tfm.set_globals(dither=True)
     # tfm.lowpass(frequency = outputformat["rate"]/2)
-    tfm.convert(samplerate = outputformat["rate"], n_channels = outputformat["channels"], bitdepth = outputformat["bits"])
+    tfm.convert(samplerate=outputformat["rate"], n_channels=outputformat["channels"], bitdepth=outputformat["bits"])
     temp_file = tempfile.mktemp(".raw")
     tfm.build_file(source, temp_file)
     return temp_file
 
+
 def convert_raw_to_zcm(raw_file: str, output_file: str, outputformat: dict) -> None:
-    samplerate = outputformat["rate"]
-    bits = outputformat["bits"]
-    channels = outputformat["channels"]
-    vera_rate = int((samplerate/(25e6 / 65536))+1)
-    adjusted_samplerate = int(vera_rate * (25e6 / 65536))
-    vera_16_bit = 1<<5 if bits==16 else 0
-    vera_stereo = 1<<4 if channels==2 else 0
     rawdata = open(raw_file, "rb").read()
     size = len(rawdata)
-    if size > MAX_SIZE:
-        rawdata = rawdata[:MAX_SIZE]
+    if size > ZcmHeader.MAX_SIZE:
+        rawdata = rawdata[:ZcmHeader.MAX_SIZE]
         size = len(rawdata)
-        print("Warning: sample data truncated to", size, "bytes!")
-    header = struct.pack("<HBHBBB", 0, 0, size & 0xffff, size>>16,
-                         0x0f | vera_16_bit | vera_stereo, vera_rate)
+        print(f"Warning: sample data truncated to {size} bytes!")
+    header = ZcmHeader.from_config(outputformat["rate"], outputformat["bits"], outputformat["channels"], size)
     with open(output_file, "wb") as out:
-        out.write(header)
+        out.write(header.header_bytes)
         out.write(rawdata)
-    os.remove(raw_file)
-    duration = len(rawdata)/channels/samplerate
-    if bits==16:
-        duration /= 2
-    print("Wrote", output_file, len(header)+len(rawdata), "bytes")
-    print(f"   {channels} channels, {bits} bits, {adjusted_samplerate} hz ({round(duration, 3)} sec.)")
+    print(f"Wrote {output_file} {len(header.header_bytes)+len(rawdata)} bytes")
+    print(f"   {header.channels} channels, {header.bits} bits, {header.sample_rate} hz ({round(header.duration, 3)} sec.)")
+    if header.sample_rate != outputformat["rate"]:
+        print("Note: actual sample rate differs from requested sample rate (due to Vera clock).")
 
-if __name__=="__main__":
-    parser = argparse.ArgumentParser(description="convert audio to zcm")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="convert audio to zcm digi")
     parser.add_argument("-s", "--stereo", action="store_true", help="stereo output, default is mono")
     parser.add_argument("-r", "--rate", type=int, help="output sample rate, default is %(default)s", default=22050)
     parser.add_argument("-8", "--eight", action="store_true", help="8 bits output, default is 16")
+    parser.add_argument("-o", "--output", type=str, help="provide your own output file name")
     parser.add_argument("--raw", action="store_true", help="input file is raw pcm already, just add zcm header")
     parser.add_argument("inputfile")
     args = parser.parse_args()
@@ -65,10 +117,8 @@ if __name__=="__main__":
         "rate": args.rate,
         "bits": 8 if args.eight else 16
     }
-    output_file = (os.path.splitext(os.path.split(args.inputfile)[1])[0] + ".zcm").upper()
-    if args.raw:
-        raw_file = args.inputfile
-    else:
-        raw_file = convert_source_to_raw(args.inputfile, outputformat)
+    output_file = args.output if args.output else (os.path.splitext(os.path.split(args.inputfile)[1])[0] + ".zcm").upper()
+    raw_file = args.inputfile if args.raw else convert_source_to_raw(args.inputfile, outputformat)
     convert_raw_to_zcm(raw_file, output_file, outputformat)
-
+    if not args.raw:
+        os.remove(raw_file)
--- tools/zcmplay.py
+++ tools/zcmplay.py
@@ -1,59 +1,102 @@
 #!/usr/bin/env python
 
-# shows info on a ZCM digi file and optionally plays the audio as well.
-# Requires python 'sox' module to be installed, and the sox command line tool.
+# Shows info on a ZCM digi file and optionally plays the audio as well.
+# Requires python 'sox' module to be installed (pip install sox), and the sox command line tool.
 
-import sys
-import os
 import struct
 import tempfile
 import argparse
 import sox
 
 
-class ZcmFile:
-    def __init__(self, filename: str) -> None:
-        with open(filename, "rb") as fin:
-            addr, bank, self.size, size_hi, vera_cfg, vera_rate = struct.unpack("<HBHBBB", fin.read(8))
-            self.sixteen_bit = bool(vera_cfg & 1<<5)
-            self.stereo = bool(vera_cfg & 1<<4)
-            self.volume = vera_cfg & 0x0f
-            self.size += size_hi << 16
-            self.pcm_bytes = fin.read()
-            self.sample_rate = int(vera_rate * (25e6 / 65536))
-            if addr!=0 or bank!=0:
-                print("Warning: the first 3 bytes of the zcm file aren't zero! (addr/bank)")
-            assert self.size == len(self.pcm_bytes)
+class ZcmHeader():
+    # note: this class is shared with other Zcm tools
+    MAX_SIZE = 2048 * 1024 - 8192  # 2 mb banked ram minus 1 bank (8kb) for kernal.
+    HEADER_FORMAT = "<HBHBBB"
 
-    def print_info(self) -> None:
-        duration = self.size / self.sample_rate
-        if self.sixteen_bit:
-            duration /= 2
-        if self.stereo:
-            duration /= 2
-        print("pcm size:", self.size, "bytes")
-        print("16 bit" if self.sixteen_bit else "8 bit", end=" ")
-        print("stereo" if self.stereo else "mono", end=", ")
-        print(self.sample_rate, "hz.")
-        print("volume:", self.volume)
-        print("duration:", round(duration, 3), "sec.")
+    def __init__(self) -> None:
+        self.sample_rate = 0
+        self.bits = 0
+        self.channels = 0
+        self.size = 0
+        self.volume = 0x0f
+        self.duration = 0.0
+        self.header_bytes = b""
 
-    def play(self) -> None:
+    @staticmethod
+    def from_config(samplerate: int, bits: int, channels: int, size: int) -> 'ZcmHeader':
+        assert bits in (8, 16)
+        assert channels in (1, 2)
+        assert 1 <= samplerate <= 48828
+        assert 0 < size <= ZcmHeader.MAX_SIZE
+        header = ZcmHeader()
+        vera_rate = header.samplerate_to_vera_rate(samplerate)
+        vera_16_bit = 1 << 5 if bits == 16 else 0
+        vera_stereo = 1 << 4 if channels == 2 else 0
+        header.sample_rate = header.vera_rate_to_samplerate(vera_rate)
+        header.bits = bits
+        header.channels = channels
+        header.size = size
+        header.volume = 0x0f
+        header.duration = size / channels / header.sample_rate
+        if bits == 16:
+            header.duration /= 2
+        header.header_bytes = struct.pack(ZcmHeader.HEADER_FORMAT, 0, 0, size & 0xffff, size >> 16,
+                                          vera_16_bit | vera_stereo | header.volume, vera_rate)
+        return header
+
+    @staticmethod
+    def from_header(header_bytes: bytes) -> 'ZcmHeader':
+        addr, bank, size_lo, size_hi, vera_cfg, vera_rate = struct.unpack(ZcmHeader.HEADER_FORMAT, header_bytes)
+        if addr or bank:
+            print("Warning: the first 3 bytes of the zcm aren't zero! (addr/bank)")
+        header = ZcmHeader()
+        header.size = size_lo + (size_hi << 16)
+        header.bits = 16 if vera_cfg & (1 << 5) else 8
+        header.channels = 2 if vera_cfg & (1 << 4) else 1
+        header.volume = vera_cfg & 0x0f
+        header.header_bytes = header_bytes
+        header.sample_rate = header.vera_rate_to_samplerate(vera_rate)
+        header.duration = header.size / header.channels / header.sample_rate
+        if header.bits == 16:
+            header.duration /= 2
+        return header
+
+    @staticmethod
+    def samplerate_to_vera_rate(hz: int) -> int:
+        return int((hz / (25e6 / 65536)) + 1)
+
+    @staticmethod
+    def vera_rate_to_samplerate(vrate: int) -> int:
+        return int(vrate * (25e6 / 65536))
+
+
+def play_zcm_file(filename: str, playback: bool = True) -> None:
+    with open(filename, "rb") as fin:
+        header = ZcmHeader.from_header(fin.read(8))
+        pcm_bytes = fin.read()
+        assert header.size == len(pcm_bytes)
+
+    print("pcm size:", header.size, "bytes")
+    print("bits:", header.bits)
+    print("channels:", header.channels)
+    print("sample rate:", header.sample_rate)
+    print("volume:", header.volume)
+    print("duration:", round(header.duration, 3), "sec.")
+
+    if playback:
         print("Playing digi.")
         with tempfile.NamedTemporaryFile(suffix=".raw") as tmpfile:
-            tmpfile.write(self.pcm_bytes)
-            sox.core.sox(["-t", "raw", "-r", str(self.sample_rate),
-                          "-b", "16" if self.sixteen_bit else "8",
-                          "-c", "2" if self.stereo else "1",
+            tmpfile.write(pcm_bytes)
+            sox.core.sox(["-t", "raw", "-r", str(header.sample_rate),
+                          "-b", str(header.bits),
+                          "-c", str(header.channels),
                           "-e", "signed-integer", tmpfile.name, "-d"])
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="display and/or play zcm digi file")
     parser.add_argument("-i", "--infoonly", action="store_true", help="only show info, don't play sound")
     parser.add_argument("zcmfile")
     args = parser.parse_args()
-    zcm = ZcmFile(args.zcmfile)
-    zcm.print_info()
-    if not args.infoonly:
-        zcm.play()
+    play_zcm_file(args.zcmfile, not args.infoonly)
